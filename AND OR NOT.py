9e8b659621c9909ed53937a54274f8bfa021dff2
all4sops2013-ui <all4sops2013@gmail.com>
2026-05-23 13:47:10 +0100

    Add files via upload

--- AND OR NOT.py
+++ AND OR NOT.py
@@ -1,3 +1,4 @@
+
 #Write a program to check whether the given values have boolean values or not.
 
 values = int(input("Enter a number between 1 and 10: "))
@@ -19,3 +20,16 @@ if not(num_1 - num_2 == 7):
     print("The difference between num_1 and num_2 is NOT 7")
 elif not(num_1 % num_2 == 0):
     print("The answer for num_1 and num_2 is NOT equal to 0")
+
+
+
+# task 3
+#Write a program to calculate the BMI of a person?
+
+#BMI = weight (kg) ÷ [height (m)]²
+
+weight = float(input("enter your weight in kg: "))
+height = float(input("enter your height in m: "))
+
+BMI = ((weight)/(height)**2)
+print(BMI)
