743e156d10310b73fe4a27b7d800e565923eeb19
all4sops2013-ui <all4sops2013@gmail.com>
2026-05-23 13:15:21 +0100

    Add files via upload

---
+++ AND OR NOT.py
@@ -0,0 +1,8 @@
+#Write a program to check whether the given values have boolean values or not.
+
+values = int(input("Enter a number between 1 and 10: "))
+
+if values == 4 or values == 2:
+    print("this is a boolean value")
+else:
+    print("not a boolean value")
